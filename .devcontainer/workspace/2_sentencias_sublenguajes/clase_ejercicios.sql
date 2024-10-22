91a4d6a44a4ae3a0ee1ed86fb419c3a0033977a8
Anderson Ocaña <andru.ocatorres@gmail.com>
2024-10-21 21:36:36 -0300

    cambios

---
+++ .devcontainer/workspace/2_sentencias_sublenguajes/clase_ejercicios.sql
@@ -0,0 +1,9 @@
+-- DQL --> DATA QUERY LANGUAGE
+
+-- RETRIEVING DATA  -> READ DATA
+
+-- FUNCIONES BUILT IN MYSQL
+-- https://dev.mysql.com/doc/refman/8.4/en/functions.html
+
+
+SELECT id_class, description FROM coderhouse_gamers.CLASS;
